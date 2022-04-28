4f44f844079a4aff227bb5341754e23a835f9abb
stlvce <egorglv@mail.ru>
2022-04-28 16:53:56 +0500

    3.2 Модуль авторизации

--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+from .forms import UserForm
 
-# Create your views here.
+
+@login_required
+def register(request): 
+    if request.method == 'POST': 
+        form = UserForm(request.POST) 
+        if form.is_valid(): 
+            user = form.save() 
+            login(request, user) 
+            return redirect('index')
+
+        return render(request, 'register.html', {'form': form}) 
+     
+    form = UserForm() 
+    return render(request, 'register.html', {'form': form})
+
+def user_login(request):
+    form = UserForm()
+    if request.method == 'POST':
+        user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
+        if user:
+            login(request, user)
+            return redirect('index')
+        
+        return render(request, 'login.html', {'form': form, 'invalid': True})
+
+    return render(request, 'login.html', {'form': form})
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect('login')
