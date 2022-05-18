936d8c76395564cf7be814b45f7916afe968ddfd
stlvce <egorglv@mail.ru>
2022-05-19 01:13:26 +0500

    create app frontend

--- clicker/urls.py
+++ clicker/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('backend/', include('backend.urls')),
+    path('frontend', include('frontend.urls')),
 ]
